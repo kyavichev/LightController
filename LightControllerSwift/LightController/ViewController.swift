06d3be0a09672e2362af1ccb9c795dba361ddbc1
Konstantin <konstantin@lumoslabs.com>
2016-07-05 01:00:37 -0700

    Light Controller app update

--- LightControllerSwift/LightController/ViewController.swift
+++ LightControllerSwift/LightController/ViewController.swift
@@ -7,20 +7,85 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController
+class ViewController: UIViewController, UITextFieldDelegate
 {
-    static let ipString = "http://10.0.1.10:3000/"
+    static var ipString = "http://10.0.1.10:3000/"
+    
+    @IBOutlet weak var connectionLabel: UILabel!
+    @IBOutlet weak var urlTextField: UITextField!
     
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.urlTextField.delegate = self;
+        
+        NSTimer.scheduledTimerWithTimeInterval(10.0, target: self, selector: #selector(ViewController.checkHeartBeat), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func checkHeartBeat()
+    {
+        NSLog("Checking heartbeat")
+        
+//        var completionHandler:(String)->Void = {
+//            (path:String) -> Void in
+//            NSLog( "Poop deck" );
+//        }
+        
+        
+        
+        let completionHandler:(NSData?, NSURLResponse?, NSError?) -> Void = { (data, response, error:NSError?) in
+            
+            if let httpResponse = response as? NSHTTPURLResponse {
+                print("error \(httpResponse.statusCode)")
+            }
+            
+            if ( error != nil )
+            {
+                NSLog ( "Cannot connect" )
+                print( "Error: \(error)" )
+                
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.connectionLabel.text = "not connected"
+                })
+                
+            }
+            else
+            {
+                NSLog ( "Connected" )
+                
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.connectionLabel.text = "connected"
+                })
+            }
+        }
+        
+        self.sendGetRequest3( "heartBeat", completionHandler:completionHandler)
+    }
+    
+    
+    @IBAction func urlTextFieldFinishedEditting(textField: UITextField)
+    {
+        NSLog( "New value: " + textField.text! )
+        ViewController.ipString = "http://" + textField.text! + "/"
+    }
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool
+    {
+        self.view.endEditing(true)
+        return false
     }
     
     
@@ -141,6 +206,22 @@ class ViewController: UIViewController
         task.resume()
     }
     
+    
+    func sendGetRequest3 ( path:String, completionHandler:(NSData?, NSURLResponse?, NSError?) -> Void )
+    {
+        let requestString = ViewController.ipString + path
+        NSLog( "Sending request: %@", requestString );
+        
+        let url : NSURL = NSURL(string: requestString)!
+        let request: NSURLRequest = NSURLRequest(URL: url)
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler: completionHandler );
+        
+        task.resume()
+    }
+    
     func sendPostRequest ( path:String, params: Dictionary<String, String> )
     {
         let requestString = ViewController.ipString + path;
@@ -181,5 +262,31 @@ class ViewController: UIViewController
         
         task.resume()
     }
+//    
+//    func sendHeartBeatRequest ()
+//    {
+//        let requestString = ViewController.ipString + "heartbeat";
+//        NSLog( "Sending request: %@", requestString );
+//        
+//        let url : NSURL = NSURL(string: requestString)!
+//        let request: NSURLRequest = NSURLRequest(URL: url)
+//        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+//        let session = NSURLSession(configuration: config)
+//        
+//        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler: {(data, response, error : NSError?) in
+//            
+//            if ( error != nil )
+//            {
+//                print( "Error: \(error)" );
+//            }
+//            
+//            // notice that I can omit the types of data, response and error
+//            
+//            // your code
+//            
+//        });
+//        
+//        task.resume()
+//    }
 }
 
